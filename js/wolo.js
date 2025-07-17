98614031179ac922115de1b33185514b767107f4
choi ej <cyslisa0211@naver.com>
2025-07-17 14:45:55 +0900

    250717-1

---
+++ js/wolo.js
@@ -0,0 +1,236 @@
+gsap.registerPlugin(ScrollTrigger)
+
+/* 헤더 */
+const headerGnb = document.querySelector('#gnb')
+const header = document.querySelector('header')
+const gnbDep1 = document.querySelectorAll('#gnb .dep1>li')
+const menu = document.querySelector('.header-util .menu-wrap');
+const langWrap = document.querySelector('.header-util .lang-wrap')
+const langBtn = document.querySelector('.header-util .lang-wrap button')
+/** 
+window.innerWidth
+마우스오버 이벤트 => 버튼은 예외
+*/
+const hBreakpoint = 1440;
+if (window.innerWidth >= hBreakpoint) {
+    headerGnb.addEventListener('mouseenter', function() {
+        header.classList.add('scroll-a')
+        header.classList.add('scroll')
+    });
+    headerGnb.addEventListener('mouseleave', function() {
+        header.classList.remove('scroll-a')
+        if(window.scrollY >= header.offsetHeight) {
+            header.classList.add('scroll');
+        } 
+        else {
+            header.classList.remove('scroll')
+        }
+    });
+} 
+
+
+window.addEventListener('scroll',() => {
+    console.log(window.scrollY);
+    if(window.scrollY >= header.offsetHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
+
+langBtn.addEventListener('click', () => {
+    langWrap.classList.toggle('active');
+})
+
+if (window.innerWidth < hBreakpoint) {
+    menu.addEventListener('click',function(){
+    if (menu.classList.contains('open')) {
+        menu.classList.remove('open');
+        if(window.scrollY >= header.offsetHeight) {
+            header.classList.add('scroll')
+        } else {
+            header.classList.remove('scroll')
+        }
+        header.classList.remove('scroll-a');
+    } else {
+        menu.classList.add('open');
+        header.classList.add('scroll');
+        header.classList.add('scroll-a');
+    }
+});
+}
+
+
+// window.addEventListener('resize', checkScreenSize);
+
+/* 푸터 */
+const familyBtn = document.querySelector('.family-link .family-site button')
+const familyList = document.querySelector('.family-link .family-site')
+familyBtn.addEventListener('click', function() {
+    familyList.classList.toggle('active')
+})
+
+/* 섹션 */
+const section = gsap.utils.toArray('#main-page section');
+// => Node List 형태의 유사 배열을 배열로 변환
+
+section.forEach(section => {
+    gsap.from(section, {
+        y: 100, opacity: 0, duration: 0.5,
+        scrollTrigger: {
+            trigger: section,
+            start: 'top 70%',
+        }
+    })
+});
+
+/* 메인비주얼 */
+const totalSlides = document.querySelectorAll('.swiper-slide').length;
+console.log('슬라이드 개수:', totalSlides);
+let progressTimeout;
+let animationDuration = 3000;
+const playBtn = document.querySelector('.btn-play');
+const stopBtn = document.querySelector('.btn-stop');
+
+document.addEventListener("DOMContentLoaded", () => {});
+    
+const mainSwiper = new Swiper(".main-swiper", {
+loop: true,
+autoplay: { delay: animationDuration, disableOnInteraction: false },
+pagination: { el: '.swiper-pagination', clickable: true },
+effect: 'slide',
+observer: true, observeParents: true,
+on: { slideChangeTransitionEnd: ensurePlay }
+});
+
+playBtn.addEventListener('click', () => {
+    mainSwiper.autoplay.start(); 
+    stopBtn.style.display = 'flex';
+    playBtn.style.display = 'none';
+});
+
+stopBtn.addEventListener('click', () => {
+    mainSwiper.autoplay.stop(); 
+    stopBtn.style.display = 'none';
+    playBtn.style.display = 'flex'; 
+});
+
+
+
+
+
+
+/* 판매량 */
+
+function countUp(selector, target, duration = 1500) {
+    const el = document.querySelector(selector);
+    if (!el) return;
+
+    let start = 0;
+    const end = parseInt(target, 10);
+    const frameRate = 60;
+    const totalFrames = Math.round(duration / (1000 / frameRate)); 
+    const increment = end / totalFrames; 
+    
+    const counter = setInterval(() => {
+      start += increment;
+      if (start >= end) {
+        clearInterval(counter);
+        el.textContent = end.toLocaleString();
+      } else {
+        el.textContent = Math.floor(start).toLocaleString();
+      }
+    }, 1000 / frameRate);
+  }
+  window.addEventListener('DOMContentLoaded', () => {
+    ScrollTrigger.create({
+    trigger: '.compet-wrap',
+    start: 'top 70%',
+    onEnter: () => {
+      countUp('.n1', 1, 500);
+      countUp('.n2', 100);
+      countUp('.n3', 40);
+      countUp('.n4', 1600);
+      }
+    }); 
+  });
+
+/* 히스토리 */
+
+const history = gsap.timeline({
+    
+  scrollTrigger: {
+        trigger: '.history',
+        start: 'top 40%'
+    }
+});
+history.from('.history li:nth-child(1)', {opacity: 0, x: 1000, duration: 0.3})    
+        .from('.history li:nth-child(2)', {opacity: 0, x: 1000, duration: 0.3}) 
+        .from('.history li:nth-child(3)', {opacity: 0, x: 1000, duration: 0.3}) 
+        .from('.history li:nth-child(4)', {opacity: 0, x: 1000, duration: 0.3}) 
+        .from('.history li:nth-child(5)', {opacity: 0, x: 1000, duration: 0.3}) 
+        .from('.history li:nth-child(6)', {opacity: 0, x: 1000, duration: 0.3}) 
+    
+
+/* 브랜드 */
+
+
+const brand = gsap.utils.toArray('.brand-list');
+
+brand.forEach(brand => {
+    gsap.from(brand, {
+        y: 100, opacity: 0, delay: 1, duration: 0.5,
+        scrollTrigger: {
+            trigger: '.brand .container',
+            start: 'top 30%',
+        }
+    })
+});
+
+
+/* 비전 */
+
+const swiperVision = new Swiper(".way-swiper", {
+        loop: true,
+        autoplay: {delay: 0},
+        slidesPerView: 'auto',
+        speed: 4000,
+        spaceBetween: 40,
+    });
+
+const way1p = gsap.timeline({
+  scrollTrigger: {
+        trigger: '.way-1p',
+        start: 'top 50%'
+    }
+});
+way1p.from('.way-1p img', {scale: 0.95,opacity: 0})
+    .from('.way-1p .txt', {opacity: 0, x: 1000})
+
+const way2p = gsap.timeline({
+  scrollTrigger: {
+        trigger: '.way-2p',
+        start: 'top 50%'
+    }
+});
+way2p.from('.way-2p img', {scale: 0.95,opacity: 0})
+    .from('.way-2p .txt', {opacity: 0, x: -1000})
+    
+
+
+/* 뉴스 */
+
+
+
+const news = gsap.timeline({
+  scrollTrigger: {
+        trigger: '.news',
+        start: 'top 40%'
+    }
+});
+news.from('.news-list li:nth-child(1)', {opacity: 0, x: 1000, duration: 0.3})    
+    .from('.news-list li:nth-child(2)', {opacity: 0, x: 1000, duration: 0.3}) 
+    .from('.news-list li:nth-child(3)', {opacity: 0, x: 1000, duration: 0.3}) 
+
+
+/* 인재채용 */
